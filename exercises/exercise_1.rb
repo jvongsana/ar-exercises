592d0baab715214922cb3d58a71f16c0db42282c
Jason <jvongsana@hotmail.com>
2020-11-09 14:31:18 -0800

    Created three store entries in the database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,6 +7,6 @@ puts "----------"
 
 Store.create(name: "Burnaby", annual_revenue: 300000.00, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000.00, mens_apparel: false, womens_apparel: true)
-Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Gastown", annual_revenue: 190000.00, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
