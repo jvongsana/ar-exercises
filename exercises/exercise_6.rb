c8ffbdff393f1aeefcac34ea5a66e4d43355155d
Jason <jvongsana@hotmail.com>
2020-11-09 15:04:30 -0800

    associated employees with stores and created new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+Store.has_many :employees
+Employee.belongs_to :Store
+
+
+@store1.employees.create(first_name: "Jason", last_name: "Vongsana", hourly_rate: 60)
+@store1.employees.create(first_name: "Jennie", last_name: "Kim", hourly_rate: 60)
+@store1.employees.create(first_name: "Jisoo", last_name: "Kim", hourly_rate: 60)
+@store2.employees.create(first_name: "Lalisa", last_name: "Manoban", hourly_rate: 60)
+@store2.employees.create(first_name: "Chaeyoung", last_name: "Park", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+
+puts Employee.count
+puts @store1.employees.inspect
+puts @store2.employees.inspect
